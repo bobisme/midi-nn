1788e7301637a126dfa7931ee8145ae8843eab0d
Bob <bob@standardinput.co>
2024-01-19 14:27:28 -0500

    move from notebook to model.py and train.py

---
+++ model.py
@@ -0,0 +1,211 @@
+import math
+import random
+from dataclasses import dataclass
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+assert torch.cuda.is_available()
+# random.seed(42)
+# torch.manual_seed(42)
+
+
+@dataclass
+class ModelConfig:
+    context_len: int = 512
+    n_layers: int = 12
+    n_heads: int = 8
+    n_embeds: int = 32
+    n_tokens: int = 261
+    n_added_params: int = 4
+    dropout: float = 0.0
+    bias: bool = True
+
+    @property
+    def n_embed_and_added(self):
+        return self.n_embeds + self.n_added_params
+
+    @property
+    def n_tokens_and_added(self):
+        return self.n_tokens + self.n_added_params
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, config: ModelConfig, flash=True):
+        super().__init__()
+        assert config.n_embeds % config.n_heads == 0
+        self.config = config
+        self.flash = flash
+        head_size = config.n_embeds // config.n_heads
+        self.key = nn.Linear(config.n_embeds, head_size, bias=config.bias)
+        self.query = nn.Linear(config.n_embeds, head_size, bias=config.bias)
+        self.value = nn.Linear(config.n_embeds, head_size, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout)
+        self.register_buffer(
+            "tril", torch.tril(torch.ones(config.context_len, config.context_len))
+        )
+
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        v = self.value(x)
+        if self.flash:
+            dropout = self.config.dropout if self.training else 0.0
+            return F.scaled_dot_product_attention(
+                q, k, v, attn_mask=None, dropout_p=dropout, is_causal=True
+            )
+        w = q @ k.transpose(-2, -1) * C**-0.5
+        w = w.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
+        w = F.softmax(w, dim=-1)
+        w = self.dropout(w)
+        return w @ v
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+        self.config = config
+        head_size = config.n_embeds // config.n_heads
+        self.heads = nn.ModuleList(
+            [SelfAttention(config) for _ in range(config.n_heads)]
+        )
+        self.projection = nn.Linear(
+            config.n_heads * head_size, config.n_embeds, bias=False
+        )
+        self.dropout = nn.Dropout(config.dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.projection(out)
+        out = self.dropout(out)
+        return out
+
+
+class FeedForward(nn.Module):
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+        self.config = config
+        self.net = nn.Sequential(
+            nn.Linear(config.n_embeds, 4 * config.n_embeds),
+            nn.ReLU(),  # TODO: GELU
+            nn.Linear(4 * config.n_embeds, config.n_embeds),
+            nn.Dropout(config.dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class SABlock(nn.Module):
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+        head_size = config.n_embeds // config.n_heads
+        self.sa = MultiHeadAttention(config)
+        self.ffwd = FeedForward(config)
+        self.ln1 = nn.LayerNorm(config.n_embeds)
+        self.ln2 = nn.LayerNorm(config.n_embeds)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
+
+class Model(nn.Module):
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+        self.config = config
+        self.token_embedding_table = nn.Embedding(config.n_tokens, config.n_embeds)
+        self.position_embedding_table = nn.Embedding(
+            config.context_len, config.n_embeds
+        )
+        self.drop = nn.Dropout(config.dropout)
+        self.blocks = nn.Sequential(*[SABlock(config) for _ in range(config.n_layers)])
+        self.layer_norm = nn.LayerNorm(config.n_embed_and_added, bias=config.bias)
+        self.lm_head = nn.Linear(
+            config.n_embed_and_added, config.n_tokens_and_added, bias=False
+        )
+        # Tie weights
+        print(f"{self.token_embedding_table.weight.shape=}")
+        print(f"{self.lm_head.weight.shape=}")
+        tie_weights = torch.nn.Parameter(
+            self.lm_head.weight[: config.n_tokens, : config.n_embeds]
+        )
+        print(f"{tie_weights.shape=}")
+        self.token_embedding_table.weight = tie_weights
+        self.apply(self._init_weights)
+        # Scale the weights of residual layers at initialization by a factor of 1/√N
+        # where N is the number of residual layers.
+        # Language Models are Unsupervised Multitask Learners
+        for name, p in self.named_parameters():
+            if name.endswith("projection.weight"):
+                torch.nn.init.normal_(
+                    p, mean=0.0, std=0.02 / math.sqrt(2 * config.n_layers)
+                )
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, x, targets=None):
+        device = x.device
+        B, T, C = x.shape
+        assert T <= self.config.context_len, T
+        tokens = x[:, :, 0].long()
+        added = x[:, :, 1:].view(
+            -1, self.config.context_len, self.config.n_added_params
+        )
+        tok_emb = self.token_embedding_table(tokens)
+        pos_emb = self.position_embedding_table(
+            torch.arange(T, dtype=torch.long, device=device)
+        )
+        x = tok_emb + pos_emb
+        x = self.drop(x)
+        x = self.blocks(x)
+        x = torch.cat((x, added), dim=2)
+        x = self.layer_norm(x)
+        if targets is None:
+            # only use last position for generation
+            logits = self.lm_head(x[:, [-1], :])
+            return logits, None, None, None
+        logits = self.lm_head(x)
+        B, T, C = logits.shape
+        classes = targets[:, :, 0].long()
+        added_params = targets[:, :, 1:]
+        label_loss = F.cross_entropy(logits.view(B * T, C), classes.view(B * T))
+        sq_err = (
+            added_params[:, :, -self.config.n_added_params :]
+            - logits[:, :, -self.config.n_added_params :]
+        ) ** 2
+        added_param_loss = sq_err.mean(dim=1).mean(dim=0).sum()
+        loss = label_loss + added_param_loss
+        return logits, loss, label_loss, added_param_loss
+
+    @torch.no_grad()
+    def generate(self, idx, max_new_tokens, temperature=1.0, top_k=None):
+        self.eval()
+        out = []
+        for _ in range(max_new_tokens):
+            idx = idx[:, -self.config.context_len :]  # truncate context
+            logits, _, _, _ = self(idx)
+            # TODO: temperature and top-k
+            probs = F.softmax(logits[:, -1, : self.config.n_tokens], dim=-1)
+            next_token = torch.multinomial(probs, num_samples=1)  # B, 1
+            token = next_token.int().item()
+            added_params = logits[:, -1, -self.config.n_added_params :].view(
+                1, self.config.n_added_params
+            )
+            next_idx = torch.cat((next_token, added_params), dim=-1)  # B, 3
+            out.append([token] + added_params.tolist()[0])
+            idx = torch.cat(
+                (idx, next_idx.view(1, -1, 1 + self.config.n_added_params)), dim=1
+            )
+            if next_token.item() == 3:
+                break
+        return out
---
+++ train.py
@@ -0,0 +1,222 @@
+from contextlib import contextmanager
+import glob
+from pprint import pp
+import sys
+
+import cbor2
+import torch
+import matplotlib.pyplot as plt
+from torch.functional import Tensor
+from torch.utils.tensorboard.writer import SummaryWriter
+
+from model import Model, ModelConfig
+
+BATCH_SIZE = 128
+RUN_KEY = "attention-v5"
+summary = SummaryWriter(f"runs/{RUN_KEY}")
+
+TrackElement = tuple[int, float, float, float, float]
+
+
+class ModelStats:
+    def __init__(self, model: Model):
+        self.model = model
+
+    def estimate_flops(self):
+        pass
+
+    def num_parameters(self):
+        n_elements = sum(p.numel() for p in self.model.parameters())
+        return n_elements
+
+
+def get_batch(data: torch.Tensor, config: ModelConfig, batch_size=BATCH_SIZE):
+    ix = torch.randint(len(data) - config.context_len, (batch_size,))
+    x = torch.stack([data[i : i + config.context_len] for i in ix])
+    y = torch.stack([data[i + 1 : i + config.context_len + 1] for i in ix])
+    return x.cuda(), y.cuda()
+
+
+@contextmanager
+def autocast_ctx():
+    dtype = (
+        torch.bfloat16
+        if torch.cuda.is_available() and torch.cuda.is_bf16_supported()
+        else torch.float16
+    )
+    with torch.amp.autocast(device_type="cuda", dtype=dtype):
+        yield
+
+
+@torch.no_grad()
+def estimate_loss(model: Model, train_data: Tensor, eval_data: Tensor, eval_iters=10):
+    out = {}
+    model.eval()
+    d = {"train": train_data, "eval": eval_data}
+    for split in ("train", "eval"):
+        data = d[split]
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(data, model.config)
+            with autocast_ctx():
+                _, loss, _, _ = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+
+def get_optimizer(
+    named_params, learning_rate=1e-3, betas=(0.9, 0.95), weight_decay=0.1
+):
+    params = {name: param for name, param in named_params if param.requires_grad}
+    decay_params = [p for p in params.values() if p.dim() >= 2]
+    nodecay_params = [p for p in params.values() if p.dim() < 2]
+    groups = [
+        {"params": decay_params, "weight_decay": weight_decay},
+        {"params": nodecay_params, "weight_decay": 0.0},
+    ]
+    return torch.optim.AdamW(groups, lr=learning_rate, betas=betas, fused=True)
+
+
+def train(
+    model: Model,
+    data,
+    optimizer,
+    scheduler,
+    global_training_steps=0,
+) -> tuple[int, float]:
+    lossi = []
+    model.train()
+    scaler = torch.cuda.amp.GradScaler()
+    max_steps = 10_000
+    for i in range(max_steps + 1):
+        global_training_steps += 1
+        Xb, Yb = get_batch(data, model.config)
+        optimizer.zero_grad(set_to_none=True)
+        with autocast_ctx():
+            logits, loss, label_loss, added_param_loss = model(Xb, Yb)
+        if i % 10 == 0:
+            summary.add_scalar("loss", loss.item(), global_training_steps)
+            summary.add_scalar("label_loss", label_loss.item(), global_training_steps)
+            summary.add_scalar(
+                "added_param_loss", added_param_loss.item(), global_training_steps
+            )
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+        # loss.backward()
+        # optimizer.step()
+        # track
+        scheduler.step()
+        if i % 100 == 0:
+            print(
+                f"{i:7d}/{max_steps}: {loss.item():.4f}, LR={scheduler.get_last_lr()[0]}"
+            )
+        lossi.append(loss.log10().item())
+    plt.figure(figsize=(16, 6))
+    r = (len(lossi) // 100) * 100
+    plt.plot(torch.tensor(lossi)[:r].view(-1, 100).mean(1))
+    plt.savefig("plot.svg", format="svg")
+    return max_steps, lossi[-1]
+
+
+def generate_samples(model: Model, sample_count=1):
+    n_added = model.config.n_added_params
+    ctx_len = model.config.context_len
+    # create context with padding + song start token
+    gen_ctx = torch.tensor(
+        [[[1, *[0.0] * n_added]] * (ctx_len - 1) + [[2, *[0.0] * n_added]]],
+        device="cuda",
+    )
+    for sample in range(sample_count):
+        generated = model.generate(gen_ctx.clone(), max_new_tokens=1_000)
+        with open(f"sample-{sample}.tokens", "wb") as f:
+            cbor2.dump(generated, f)
+            print("saved generated sample to", f.name)
+
+
+def save(
+    model: Model, optimizer, loss: float, learning_rate: float, steps_trained: int
+):
+    filename = f"{RUN_KEY}_{steps_trained}i.pt"
+    save_data = {
+        "model_state_dict": model.state_dict(),
+        "steps_trained": steps_trained,
+        "optimizer_state_dict": optimizer.state_dict(),
+        "loss": loss,
+        "learning_rate": learning_rate,
+    }
+    torch.save(save_data, filename)
+    torch.save(save_data, "checkpoint.pt")
+    print(f"saved checkpoint to {filename} and checkpoint.pt")
+
+
+def main(args: list[str]):
+    resume = len(sys.argv) > 1 and sys.argv[1] == "resume"
+    generate = len(sys.argv) > 1 and sys.argv[1] == "generate"
+
+    torch.backends.cuda.matmul.allow_tf32 = True
+    torch.backends.cudnn.allow_tf32 = True
+
+    track_paths = glob.glob("/home/bob/vmshare/midi/solo-piano/**/*.tokens")
+    tracks = []
+    for path in track_paths:
+        with open(path, "rb") as f:
+            track_data: list[TrackElement] = cbor2.load(f)
+            if len(track_data) < 100:
+                print("too short:", path)
+                continue
+            tracks.append(track_data)
+    tracks = [t for t in tracks if t]
+
+    data = torch.tensor([event for track in tracks for event in track])
+    split_n = int(0.9 * len(data))
+    train_data = data[:split_n]
+    eval_data = data[split_n:]
+
+    config = ModelConfig()
+    model = Model(config).cuda()
+    summary.add_graph(model, get_batch(train_data, config))
+
+    stats = ModelStats(model)
+    print(f"Model has {stats.num_parameters()} parameters.")
+
+    estimate = estimate_loss(model, train_data, eval_data)
+    pp(estimate)
+
+    optimizer = get_optimizer(model.named_parameters(), learning_rate=1e-3)
+    epoch_size = split_n // BATCH_SIZE
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 2 * epoch_size, gamma=0.1)
+    global_training_steps = 0
+    if resume or generate:
+        print("loading from checkpoint.pt")
+        checkpoint = torch.load("checkpoint.pt")
+        model.load_state_dict(checkpoint["model_state_dict"])
+        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+        global_training_steps = checkpoint["steps_trained"]
+        if resume:
+            model.train()
+        if generate:
+            model.eval()
+    if not generate:
+        steps_trained, loss = train(
+            model,
+            train_data,
+            optimizer,
+            scheduler,
+            global_training_steps=global_training_steps,
+        )
+        global_training_steps += steps_trained
+        save(
+            model,
+            optimizer=optimizer,
+            loss=loss,
+            learning_rate=scheduler.get_last_lr()[0],
+            steps_trained=global_training_steps,
+        )
+    generate_samples(model)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
